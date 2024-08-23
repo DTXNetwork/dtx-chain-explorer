3f0fd2b714d62dc1f2f22320c6def0dccfb3ba55
root <nlgsakib@gmail.com>
2024-08-23 16:49:40 +0000

    added miner scheme

--- fend.sh
+++ fend.sh
@@ -2,4 +2,4 @@ export HIDE_BLOCK_MINER=true
 export NODE_OPTIONS=--max-old-space-size=4096 && cd apps/block_scout_web/assets
 npm install
 node_modules/webpack/bin/webpack.js --mode production
-cd  cd ../../../../../
+cd ../../../../../
